a86941edab93b726b65f072b330e402fa504aa48
avabob <1259395049@qq.com>
2025-04-14 08:55:49 +0800

    2025/4/13

--- 副本/Sbox/Step1_Create_Trail.py
+++ 副本/Sbox/Step1_Create_Trail.py
@@ -3,7 +3,7 @@ from sbox import Sbox
 
 def input_sbox():
 	# 输入sbox字符串
-	sbox_input = input("S盒: ")
+	sbox_input = input("请输入S盒: ")
 	# 移除方括号并按逗号分割字符串
 	sbox_input = sbox_input.strip()[1:-1]  # 去掉两边的方括号
 	# 将每个十六进制数转换为整数
